eee1cc74950c7c861c18a2dc642ebd13b6d9e4fa
IChoMo <92058713+IChoMo@users.noreply.github.com>
2023-08-04 11:04:38 -0700

    Sun continues moving after sun set
    Fixes the issue where the sun snaps to 185 degrees, and instead smooth sets the sun and continues it moving all night long till sun rise.
    
    IMPORTANT NOTE: sun set time must be in 24 hours now

--- RealWorldDayNight.cs
+++ RealWorldDayNight.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class RealWorldDayNight : MonoBehaviour
 {
+    public GameObject Sun;
+    public float SunRiseTime = 8;
+    public float SunSetTime = 20;
 
     private int CurrentHour;
     private float CurrentMinute;
-
-    public GameObject Sun;
     private Vector3 eulerRotation;
-    public float SunRiseTime = 8;
-    public float SunSetTime = 8;
     private float TimeIntoDay;
     private float SolarDayLength;
+    private float SolarNightLength;
 
     void Start()
     {
@@ -21,6 +21,8 @@ public class RealWorldDayNight : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (SunSetTime > 12) SunSetTime += 12;
+
         //calls time refresh once per frame
         CurrentHour = System.DateTime.Now.Hour;
         CurrentMinute = System.DateTime.Now.Minute;
@@ -31,27 +33,29 @@ public class RealWorldDayNight : MonoBehaviour
     void SunTime(int Hour, float Minuet)
     {
         //depending on when the sunrise and sunset is, will determin how long the day is
-        SolarDayLength = (12 - SunRiseTime) + SunSetTime;
+        SolarDayLength = SunSetTime - SunRiseTime;
+        SolarNightLength = 24 - SolarDayLength;
 
         TimeIntoDay = Hour + Minuet / 60;
 
-        var SolarEnd = SunRiseTime + SolarDayLength;
-
-        if (TimeIntoDay < SunRiseTime)
+        if (TimeIntoDay < SunRiseTime) //if the time is beofre sun rise
         {
-            Sun.transform.rotation = Quaternion.Euler(185, eulerRotation.y, eulerRotation.z);
+            Sun.transform.rotation = Quaternion.Euler(TimeIntoDay * 90 / SolarNightLength + 270, eulerRotation.y, eulerRotation.z);
         }
-        else if (TimeIntoDay > SolarEnd)
+        else if(TimeIntoDay > SunSetTime) //if the time is after sun set
         {
-            Sun.transform.rotation = Quaternion.Euler(185, eulerRotation.y, eulerRotation.z);
-        }
-        else
-        {
-            //if time is in solar window
-            var cTime = TimeIntoDay - SunRiseTime;
+            //Get the amount of hours that you currently are into the Solar Night by removing the amount of hours before the sun set
+            var SolarNightTime = TimeIntoDay - SunSetTime;
 
-            //sets sun rotation based on 185 degrees of rotation (I find works nicely for unity's Directional light) and the percentage of time into the Solarday
-            Sun.transform.rotation = Quaternion.Euler(cTime * 185 / SolarDayLength, eulerRotation.y, eulerRotation.z);
+            Sun.transform.rotation = Quaternion.Euler(SolarNightTime * 90 / SolarNightLength + 180, eulerRotation.y, eulerRotation.z);
+        }
+        else //if time is in solar window
+        {
+            //Get the amount of hours that you currently are into the Solar Day by removing the amount of hours before the sun rose
+            var SolarDayTime = TimeIntoDay - SunRiseTime;
+
+            //sets sun rotation based on 180 degrees of rotation and the percentage of time into the Solarday
+            Sun.transform.rotation = Quaternion.Euler(SolarDayTime * 180 / SolarDayLength, eulerRotation.y, eulerRotation.z);
         }
     }
 }
